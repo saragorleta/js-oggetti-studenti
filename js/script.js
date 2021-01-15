144150fd2e5afa23376f258b42e6aa8dbbecaa72
Sara <saragorleta@yahoo.com>
2021-01-15 16:54:41 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Oggetti
+// - Creare un oggetto che descriva uno studente
+// con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso il for in tutte le
+// proprietà.
+// - Creare un array di oggetti di studenti. Ciclare su
+// tutti gli studenti e stampare per ognuno nome e
+// cognome
+// - Dare la possibilità all’utente attraverso 3 prompt
+// di aggiungere un nuovo oggetto studente
+// inserendo nell’ordine: nome, cognome e età.
+
+
+var studente={
+  'nome': 'Mario',
+  'cognome':'Rossi',
+  'eta':45
+};
+for (var key in studente){
+  console.log(studente[key]);
+};
+
+var studenti=[
+  {
+    'nome':'Mario',
+    'cognome':'Rossi'
+  },
+  {
+    'nome':'Luca',
+    'cognome':'Bianchi'
+  },
+  {
+    'nome':'Luigi',
+    'cognome':'Verdi'
+  },
+];
+for (var i=0; i<studenti.length;i++){
+  console.log(studenti[i]);
+  for(var k in studenti[i]){
+    console.log(k +' '+ classi[i][k]);
+  }
+}
+// var studenti={
+//   ['mario rossi'],
+//   ['luigi bianchi'],
+//   ['paolo verdi']
+// };
+// for (var i=0; i<studenti.length;i++){
+//   for (var j=0; j<studenti[i].length; i++){
+//     for (var y=0; j<studenti[j].length; i++){
+//       console.log(studenti[i][j][y]);
+//     }
+//   }
+// }
